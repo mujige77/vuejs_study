6b208ca82d33d120647379eac510e240305a5c92
tteogi <tteogi@daum.net>
2018-04-01 15:18:01 +0900

    section 14 completed

---
+++ src/section 14 - Adding Animations and Transitions/1/DangerAlert.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="alert alert-danger">Danger Alert</div>
+</template>
+
+<script>
+	export default {
+		name: "danger-alert"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/section 14 - Adding Animations and Transitions/1/SuccessAlert.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="alert alert-success">Success Alert</div>
+</template>
+
+<script>
+	export default {
+		name: "danger-alert"
+	}
+</script>
+
+<style scoped>
+
+</style>
