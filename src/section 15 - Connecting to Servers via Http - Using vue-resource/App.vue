5d1f6c0016ac387ba96e058ec8b17f070edd196e
tteogi <tteogi@daum.net>
2018-04-03 16:08:01 +0900

    section 15 completed

---
+++ src/section 15 - Connecting to Servers via Http - Using vue-resource/App.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label>UserName</label>
+                    <input class="form-control" type="text" v-model="user.userName">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <input class="form-control" type="text" v-model="node">
+                <br><br>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u  in users">
+                        {{ u.userName }} - {{ u.email }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: {
+                    userName : '',
+                    email: '',
+                },
+                users : [],
+                resource: {},
+                node: 'data',
+            }
+        },
+        methods: {
+            submit() {
+                // this.$http.post('data.json', this.user).then(res => {
+                //     console.log(res)
+                // }, error => {
+                //     console.log(error)
+                // });
+                // this.resource.save({}, this.user)
+                this.resource.saveAlt(this.user)
+            },
+            fetchData() {
+                // this.$http.get('data.json').then(res => {
+                //     return res.json()
+                // }, error => {
+                //     console.log(error)
+                // }).then(data => {
+                //     console.log(data)
+                //     const resultArray = [];
+                //     for(let key in data) {
+                //         resultArray.push(data[key])
+                //     }
+                //     this.users = resultArray
+                // });
+                this.resource.getData({node: this.node})
+                .then(res => {
+                    return res.json()
+                }, error => {
+                    console.log(error)
+                }).then(data => {
+                    console.log(data)
+                    const resultArray = [];
+                    for(let key in data) {
+                        resultArray.push(data[key])
+                    }
+                    this.users = resultArray
+                });
+            }
+        },
+        created() {
+            const customActions = {
+                saveAlt: {
+                    method: 'POST', url:'alternative.json'
+                },
+                getData: {
+                    method: 'GET'
+                }
+            }
+            this.resource = this.$resource('{node}.json', {}, customActions)
+        }
+    }
+</script>
+
+<style>
+</style>
