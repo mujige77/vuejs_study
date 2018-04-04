9eeada7a387c2009d6a6eb0ec95051030b5ab334
tteogi <tteogi@daum.net>
2018-04-04 20:49:29 +0900

    section 16 completed

---
+++ src/section 16 - Routing in a VueJS Application/components/user/UserEdit.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{this.$route.query.locale}}</p>
+        <p>Analytics: {{this.$route.query.q }} </p>
+        <hr>
+        <button class="btn btn-primary" @click="confirmed = true" >Confirm</button>
+        <div style="height: 700px"></div>
+        <p id="data">Some extra data</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            confirmed: false,
+        }
+    },
+    methods: {
+
+    },
+    beforeRouteLeave(to, from, next) {
+        if(this.confirmed) {
+            next()
+        }
+        else {
+            if(confirm('Are you sure?')) {
+                next()
+            }
+            else
+                next(false)
+        }
+    },
+    created() {
+        this.confirmed = false
+    }
+}
+</script>
