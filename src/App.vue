96df585b1aaf0db28ab2a455c454e08ee7d00584
tteogi <tteogi@daum.net>
2018-04-05 20:08:05 +0900

    section 18 진행중

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <!-- <a href="#" data-toggle="collapse" data-target="#toggleDemo" class="collapsed">
+            <span class="glyphicon glyphicon-cloud"></span> Submenu 1 <span class="caret pull-right"></span>
+        </a>
+        <div class="collapse" id="toggleDemo" style="height: 0px;">
+            <ul class="nav nav-list">
+                <li><a href="#">Submenu1.1</a></li>
+                <li><a href="#">Submenu1.2</a></li>
+                <li><a href="#">Submenu1.3</a></li>
+            </ul>
+        </div> -->
+        <app-header></app-header>
+        <div class="col-xs-12">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    export default {
+        components: {
+            appHeader: Header,
+        }
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px
+    }
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header bg-success">
+                    NAME
+                    <small>{Price: PRICE}</small>
+            </div>
+            <div class="card-body">
+                <div class="float-left">
+                    <input type="number"
+                    class="form-control"
+                    placeholder="Quantity">
+                </div>
+                <div class="float-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
